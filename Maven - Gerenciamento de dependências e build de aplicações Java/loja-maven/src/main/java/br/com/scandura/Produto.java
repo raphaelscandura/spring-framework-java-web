54753e3e63ef5601cfd1bb229ef190037238601a
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-10-27 13:23:30 -0300

    Gerenciando dependências no Maven

---
+++ Maven - Gerenciamento de dependências e build de aplicações Java/loja-maven/src/main/java/br/com/scandura/Produto.java
@@ -0,0 +1,4 @@
+package br.com.scandura;
+
+public class Produto {
+}
---
+++ Maven - Gerenciamento de dependências e build de aplicações Java/loja-maven/src/test/java/br/com/scandura/loja/ProdutoTest.java
@@ -0,0 +1,11 @@
+package br.com.scandura.loja;
+
+import org.junit.jupiter.api.Test;
+
+public class ProdutoTest {
+
+    @Test
+    public void test(){
+        return;
+    }
+}
