3b575f382fb3ee39a539597e059ce75bad333741
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-10-28 15:15:21 -0300

    Escalabilidade com pool de conexões

--- Java e JDBC - Trabalhando com um banco de dados/loja-virtual-repository/src/br/com/scandura/main/ConnectionFactory.java
+++ Java e JDBC - Trabalhando com um banco de dados/loja-virtual-repository/src/br/com/scandura/main/ConnectionFactory.java
@@ -1,11 +1,25 @@
 package br.com.scandura.main;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
+
+    public DataSource dataSource;
+
+    public ConnectionFactory(){
+        ComboPooledDataSource cpds = new ComboPooledDataSource();
+        cpds.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimeZone=UTC");
+        cpds.setUser("root");
+        cpds.setPassword("root");
+        cpds.setMaxPoolSize(5);
+
+        this.dataSource = cpds;
+    }
     public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimeZone=UTC", "root", "root");
+        return this.dataSource.getConnection();
     }
 }
